4aae374d974009e27e25e954ec28d58ce61d9ef5
Rahul Rimal <genuinetechnology9@gmail.com>
2021-07-21 15:35:00 +0545

    Navbar completed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-function hideTopBar()
-{
-    var topBar = document.getElementById('top-bar');
-    
-}
+// function showDropdownMenu(dropToggler) {
+//   toggler = document.getElementById(dropToggler.id);
+//   menu = toggler.nextElementSibling;
+//   menu.classList.add("show");
+// }
+
+// function hideDropdownMenu(dropToggler) {
+//   toggler = document.getElementById(dropToggler.id);
+//   menu = toggler.nextElementSibling;
+//   if (menu.classList.contains("show")) {
+//     menu.classList.remove("show");
+//   }
+// }
